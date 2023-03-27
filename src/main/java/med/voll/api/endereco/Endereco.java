f9434489c59c60eaddc9865fea25c018d3a3e3ce
karin morais <andradkarin@gmail.com>
2023-03-27 18:24:35 -0300

    Fix logradouro field name

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -19,7 +19,7 @@ public class Endereco {
     private String complemento;
 
     public Endereco(DadosEndereco dados) {
-        this.logradouro = dados.logadouro();
+        this.logradouro = dados.logradouro();
         this.bairro = dados.bairro();
         this.cep = dados.cep();
         this.cidade = dados.cidade();
