ca27746cf1f0898f75c0bc034447a03669f7d04b
karin morais <andradkarin@gmail.com>
2023-03-29 16:36:25 -0300

    Added pagination

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -6,6 +6,8 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -23,12 +25,10 @@ public class MedicosController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
         return repository
-                .findAll()
-                .stream()
-                .map(DadosListagemMedico::new)
-                .toList();
+                .findAll(paginacao)
+                .map(DadosListagemMedico::new);
     }
 
 }
