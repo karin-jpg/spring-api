8c4d4e0b261679da2f83f5990b33cbc3c6df0c78
karin morais <andradkarin@gmail.com>
2023-03-29 17:25:22 -0300

    Added medic info update

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -33,4 +33,19 @@ public class Medico {
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
     }
+
+    public void atualizar(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+
+        if (dados.endereco() != null) {
+            this.endereco.atualizar(dados.endereco());
+        }
+
+    }
 }
