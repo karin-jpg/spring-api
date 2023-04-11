bcfe01edff1e0b823707df804d1c2e7e93027484
karin morais <andradkarin@gmail.com>
2023-04-11 08:34:38 -0300

    Added subject recuperation

--- src/main/java/med/voll/api/infra/secutiry/TokenService.java
+++ src/main/java/med/voll/api/infra/secutiry/TokenService.java
@@ -1,8 +1,11 @@
 package med.voll.api.infra.secutiry;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,6 +30,22 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubject(String token) {
+
+        DecodedJWT decodedJWT;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API clinica")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("token JWT inválido ou expirado", exception);
         }
     }
 
