04cf501842155507592522b7190a64fc06ad6fcf
karin morais <andradkarin@gmail.com>
2023-04-12 17:14:37 -0300

    Fixed spring mapping

--- src/main/java/med/voll/api/controller/UsuarioController.java
+++ src/main/java/med/voll/api/controller/UsuarioController.java
@@ -6,13 +6,13 @@ import med.voll.api.domain.usuario.Usuario;
 import med.voll.api.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("usuarios")
 public class UsuarioController {
 
